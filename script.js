bc36a4c35e813d8c846d3f1815ba258a81095ccf
roboboyhot <67493019+roboboyhot@users.noreply.github.com>
2021-06-04 22:04:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function newPage() {
+  var temp = document.getElementsByTagName("template")[0];
+  var clon = temp.content.cloneNode(true);
+  document.body.appendChild(clon);
+  window.scrollTo(0,document.body.scrollHeight);
+}
+function toTop() {
+  window.scrollTo(0,0);
+}
+function align() {
+  document.getElementById("title").style.textAlign = "left";
+}
+
