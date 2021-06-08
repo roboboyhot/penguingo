7a10eaf97665a547f93ed2777a4d62d6f58d452b
roboboyhot <67493019+roboboyhot@users.noreply.github.com>
2021-06-08 08:34:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-function tell(){
 alert("Penguin Go Has Successfully Opened! Opening Recent File When You Left")
-}
 function newPage() {
   var temp = document.getElementsByTagName("template")[0];
   var clon = temp.content.cloneNode(true);
